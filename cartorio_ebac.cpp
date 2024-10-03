371b109840baf709b150cebc2c2e2f5494a5a3bc
lucianopereira00 <lucianosccp2024@gmail.com>
2024-10-03 20:25:05 -0300

    Opção de saída
    Foi adicionada ao sistema, a opção de saída.

--- cartorio_ebac.cpp
+++ cartorio_ebac.cpp
@@ -105,11 +105,12 @@ int main()
 	  system("cls");
      
       setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
-      printf("### Cartório da EBAC ###\n\n"); //inicício do menu
+      printf("--- Cartório da EBAC ---\n\n"); //inicício do menu
       printf("Escolha a opção desejada do menu:\n\n");
       printf("\t1 - Registrar nomes\n");
       printf("\t2 - Consultar nomes\n");
       printf("\t3 - Deletar nomes\n\n"); 
+      printf("\t4 - Sair do sistema\n\n");
 	  printf("Opção: "); //Fim do menu
     
       scanf("%d" , &opcao); //Armazenando a escolha do usuário
@@ -130,14 +131,16 @@ int main()
 	   deletar();
        break;
        
-     
+       case 4:
+       printf("Obrigado por utilizar o sistema!\n");
+       return 0;
+       break;
+       
        default:
        printf("Essa opção não está disponivel!\n");
 	   system("pause");
        break;
 	  }
 	}
-	
-	
 }
 
